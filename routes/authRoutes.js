013c659a08a65159a1798d5732b5b82b541410bb
asiphile <asiphilemthethethwa@gmail.com>
2024-10-30 08:33:44 +0200

    testing to see if the end points work

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,36 +1,35 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const User = require('../models/userModel'); // 
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel"); //
 const router = express.Router();
 
-
-const JWT_SECRET = '';
+const JWT_SECRET = "658767598-657549054-657";
 
 // Register Route
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     const { username, password, role } = req.body;
     const user = new User({ username, password, role });
     await user.save();
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
 // Login Route
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
 
     if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     // Generate JWT
     const token = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: "1h",
     });
 
     res.status(200).json({ token });
