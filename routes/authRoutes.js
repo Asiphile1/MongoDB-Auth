edbd96fd7a267b5ec94cf6d0a99a9d0f81231d9b
asiphile <asiphilemthethethwa@gmail.com>
2024-10-30 08:18:01 +0200

    adding auth routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel'); // 
+const router = express.Router();
+
+
+const JWT_SECRET = '';
+
+// Register Route
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password, role } = req.body;
+    const user = new User({ username, password, role });
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Login Route
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate JWT
+    const token = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
