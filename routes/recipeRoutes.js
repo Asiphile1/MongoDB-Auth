03d52752b38b20d34c4df1412b808a7fd16c6721
asiphile <asiphilemthethethwa@gmail.com>
2024-10-30 08:24:28 +0200

    protecting recipe routes using authentication

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const recipeController = require("../controllers/recipeController");
+const { authenticateToken, authorizeRoles } = require('../middleware/authMiddleware');
 
-// adding API Endpoints
-router.post("/recipes", recipeController.createRecipe);
-router.get("/recipes", recipeController.getAllRecipes);
-router.get("/recipes/:id", recipeController.getRecipeById);
-router.put("/recipes/:id", recipeController.updateRecipe);
-router.delete("/recipes/:id", recipeController.deleteRecipe);
+// Public route for all recipes
+router.get('/', recipeController.getAllRecipes);
+
+// Route requires login
+router.get('/:id', authenticateToken, recipeController.getRecipeById);
+
+// Route to create a new recipe this will only be available for logged in users
+router.post(
+  '/',
+  authenticateToken,
+  authorizeRoles('admin', 'user'), // 'user' and 'admin' roles
+  recipeController.createRecipe
+);
+
+// Route to update a recipe  (admin only)
+router.put(
+  '/:id',
+  authenticateToken,
+  authorizeRoles('admin'),
+  recipeController.updateRecipe
+);
+
+// Route to delete a recipe (only admins)
+router.delete(
+  '/:id',
+  authenticateToken,
+  authorizeRoles('admin'),
+  recipeController.deleteRecipe
+);
 
 module.exports = router;
